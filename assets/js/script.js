be84400621a9f6e82d58e833e06e59745815e35e
Sadek-Mohammed <sadek.hmask@gmail.com>
2022-06-13 03:12:49 +0200

    Form submits to my personal email

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,12 @@
       //seconds
     }, 0)
 }());
+
+const submitButton = document.getElementById("submit_form");
+const form = document.getElementById("email_form");
+form.addEventListener("submit", function (e) {
+    setTimeout(function() {
+    submitButton.value = "Sending...";
+    submitButton.disabled = true;
+    }, 1);
+});
